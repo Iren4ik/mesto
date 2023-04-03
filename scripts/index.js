d4796f9c22780b9d634d1c1850be3a9b5c45d59c
Iren4ik <k.iren98@yandex.ru>
2023-04-03 21:09:25 +0300

    Внесены корректировки, добавлен readme

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 //Открытие и закрытие формы
 let editProfileBtn = document.querySelector('.profile__intro-edit-btn');
-let closePopupBtn = document.querySelector('.edit-form__close-btn');
-let editForm = document.querySelector('.edit-form');
+let closePopupBtn = document.querySelector('.popup__close-btn');
+let editForm = document.querySelector('.popup');
 
 function openAndCloseEditForm() {
-  editForm.classList.toggle('popup_open');
+  editForm.classList.toggle('popup_opened');
 }
 
 editProfileBtn.addEventListener('click', openAndCloseEditForm);
@@ -12,18 +12,18 @@ closePopupBtn.addEventListener('click', openAndCloseEditForm);
 
 //Сохранение новых данных
 
-let nameInput = document.querySelector('.edit-form__input-name');
-let jobInput = document.querySelector('.edit-form__input-job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let nameProfile = document.querySelector('.profile__intro-name');
-let jobProfile = document.querySelector('.profile__intro-status');
-let saveProfileBtn = document.querySelector('.edit-form__save-btn');
+let jobProfile = document.querySelector('.profile__intro-job');
+let saveProfileBtn = document.querySelector('.popup__save-btn');
 
 function handleFormSubmit(evt) {
   evt.preventDefault(); 
 
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  editForm.classList.remove('popup_open');
+  editForm.classList.remove('popup_opened');
 }
 
 editForm.addEventListener('submit', handleFormSubmit); 
