00aa5ee81ad9686d740195bca28b5d128c35972e
Iren4ik <k.iren98@yandex.ru>
2023-04-05 19:42:15 +0300

    js and picture

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let editProfileBtn = document.querySelector('.profile__intro-edit-btn');
 let closePopupBtn = document.querySelector('.popup__close-btn');
-let editForm = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__input-container');
 let nameProfile = document.querySelector('.profile__intro-name');
 let nameInput = document.querySelector('.popup__input_type_name');
@@ -10,7 +10,7 @@ let jobInput = document.querySelector('.popup__input_type_job');
 //Открытие формы
 
 function openEditForm() {
-  editForm.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
   nameInput.value =  nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
@@ -18,7 +18,7 @@ function openEditForm() {
 //Закрытие формы
 
 function closeEditForm() {
-  editForm.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 //Сохранение новых данных
