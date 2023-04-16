c38e563cb2508210e409ce697b7fd560ea233c96
Iren4ik <k.iren98@yandex.ru>
2023-04-16 23:37:28 +0300

    Добавлены лайки, удаление карточек, форма добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,89 @@
-let editProfileBtn = document.querySelector('.profile__intro-edit-btn');
-let closePopupBtn = document.querySelector('.popup__close-btn');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__input-container');
-let nameProfile = document.querySelector('.profile__intro-name');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobProfile = document.querySelector('.profile__intro-job');
-let jobInput = document.querySelector('.popup__input_type_job');
+import { initialCards } from "./constants.js";
 
-//Открытие формы
+//Переменные для формы редактирования
+const editProfileBtn = document.querySelector('.profile__intro-edit-btn');
+const closePopupEditBtn = document.querySelector('.popup__close-btn_edit');
+const popupEditForm = document.querySelector('.popup_edit-form');
+const formElement = document.querySelector('.popup__input-container');
+const nameProfile = document.querySelector('.profile__intro-name');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobProfile = document.querySelector('.profile__intro-job');
+const jobInput = document.querySelector('.popup__input_type_job');
+//Переменные для работы с карточками
+const cardsTemplate = document.getElementById('elements-template');
+const cardsContainer = document.querySelector('.elements__items');
+const addCardsBtn = document.querySelector('.profile__add-btn');
+const popupAddForm = document.querySelector('.popup_add-cards');
+const closePopupAddBtn = document.querySelector('.popup__close-btn_add');
+// const captionInput = document.querySelector('.popup__input_type_caption');
+// const linkInput = document.querySelector('.popup__input_type_link');
 
+//Форма редактирования
 function openEditForm() {
-  popup.classList.add('popup_opened');
+  popupEditForm.classList.add('popup_opened');
   nameInput.value =  nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
 
-//Закрытие формы
-
 function closeEditForm() {
-  popup.classList.remove('popup_opened');
+  popupEditForm.classList.remove('popup_opened');
 }
-
-//Сохранение новых данных
 
 function handleFormSubmit(evt) {
   evt.preventDefault(); 
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
   closeEditForm();
+} 
+
+//Добавление карточек
+//Создаем элемент
+const createCardElement = (cardData) => {
+  //ищем и клонируем контент шаблона
+  const cardElement = cardsTemplate.content.querySelector('.elements__element').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.elements__photo');
+  const cardTitle = cardElement.querySelector('.elements__place');
+  const cardDeleteBtn = cardElement.querySelector('.elements__trash');
+  const cardLikeBtn = cardElement.querySelector('.elements__like');
+
+  //присваиваем контенту значения массива с сервера
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+
+  const handleDelete = () => {
+    cardElement.remove();
+  };
+
+  const handleLike = () => {
+    cardLikeBtn.classList.toggle('elements__like_active');
+  };
+
+  cardDeleteBtn.addEventListener('click', handleDelete);
+  cardLikeBtn.addEventListener('click', handleLike);
+
+  return cardElement;
+};
+
+//Смотрим каждый элемнт массива и выводим на страницу
+initialCards.forEach((card) => {
+  const element = createCardElement(card);
+  cardsContainer.prepend(element);
+});
+
+function openAddForm() {
+  popupAddForm.classList.add('popup_opened');
 }
 
+function closeAddForm() {
+  popupAddForm.classList.remove('popup_opened');
+}
+
+
+//Слушатели формы редактирования
 editProfileBtn.addEventListener('click', openEditForm);
-closePopupBtn.addEventListener('click', closeEditForm);
+closePopupEditBtn.addEventListener('click', closeEditForm);
 formElement.addEventListener('submit', handleFormSubmit); 
+//Слушатели добавления карточек
+addCardsBtn.addEventListener('click', openAddForm);
+closePopupAddBtn.addEventListener('click', closeAddForm);
